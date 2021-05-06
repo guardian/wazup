b1b162f83e2954f9a16c94a1fe9285b3050e05ba
Kate Whalen <whalenka88@gmail.com>
2021-05-06 18:12:40 +0100

    Switch to ZIO and use Blocking thread pool

--- src/main/scala/com/gu/wazup/Main.scala
+++ src/main/scala/com/gu/wazup/Main.scala
@@ -2,6 +2,7 @@ package com.gu.wazup
 
 import com.gu.wazup.aws.AWS
 import software.amazon.awssdk.regions.Region
+import zio.blocking.Blocking
 import zio.console._
 import zio.{ExitCode, URIO, ZIO}
 
@@ -11,17 +12,17 @@ object Main extends zio.App {
   private val systemsManagerClient = AWS.systemsManagerClient("infosec", Region.of("eu-west-1"))
   private val cloudwatchClient = AWS.cloudwatchClient("infosec", Region.of("eu-west-1"))
 
-  def run(args: List[String]): URIO[Console, ExitCode] =
+  def run(args: List[String]): URIO[Console with Blocking, ExitCode] =
     wazup.forever.exitCode
 
   private val bucket = "BUCKET"
   private val prefix = "REPO/wazuh/etc/"
 
-  val wazup: ZIO[Console, Serializable, Unit] = {
+  val wazup: ZIO[Console with Blocking, Serializable, Unit] = {
     val result = for {
       wazuhFiles <- Logic.fetchFiles(s3Client, bucket, prefix)
       parameters <- Logic.fetchParameters(systemsManagerClient, "STACK/STAGE/")
-      wazuhParameters = Logic.parseParameters(parameters)
+      wazuhParameters = Logic.parseParameters(parameters, prefix)
       nodeType = Logic.getNodeType("ADDRESS", wazuhParameters)
       newConf <- Logic.createConf(wazuhFiles, wazuhParameters, nodeType)
       currentConf <- Logic.getCurrentConf("/var/ossec/etc/")
