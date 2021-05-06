eb3bfa6caf100e8f0328edd15255b57c7d613db3
Kate Whalen <whalenka88@gmail.com>
2021-05-06 12:31:08 +0100

    Revise signatures of methods, add NodeType to models

--- src/main/scala/com/gu/wazup/Logic.scala
+++ src/main/scala/com/gu/wazup/Logic.scala
@@ -1,29 +1,45 @@
 package com.gu.wazup
 
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.ssm.SsmAsyncClient
 import zio.IO
 
 
 object Logic {
   // TODO: change left to be a failure so we can return more information
-  def fetchConf(bucket: String, path: String): IO[String, WazuhConf] = {
+  def fetchFiles(client: S3Client, bucket: String, prefix: String): IO[String, WazuhFiles] = {
     ???
-    // 1. fetch parameters from parameter store
-    // 2. fetch files from S3 bucket and (optional) path
+  }
+
+  def fetchParameters(client: SsmAsyncClient, prefix: String): IO[String, Map[String, String]] = {
+    ???
+
+  }
+
+  def parseParameters(parameters: Map[String, String]): WazuhParameters = {
+    ???
   }
 
   // TODO: ossec.conf should be valid XML, can we return XML or add a method to validate the conf?
-  def interpolateOssecConf(parameters: WazuhParameters, ossecConf: String): IO[String, String] = ???
+  def createConf(wazuhFiles: WazuhFiles, parameters: WazuhParameters, nodeType: NodeType): IO[String, WazuhFiles] = {
+    ???
+  }
 
-  // if it is the first time the service is running, the current files will be None
+  // Only one instance should ingest logs from GCP and AWS
+  private[logic] def removeWodleSections(ossecConf: String): String = {
+    ???
+  }
+
+  // if it is the first time the service is running, the current files may be None
   // how do we tell the difference between populated and unpopulated files
   // TODO: decide if current conf should always be read from disk or cached?
-  def getCurrentConf: IO[String, Option[WazuhFiles]] = {
+  def getCurrentConf(path: String): IO[String, WazuhFiles] = {
     ???
   }
 
   // if it is the first time the service is running, the current files will be None
   // TODO: consider case class that represents the interpolation
-  def shouldUpdate(incoming: WazuhFiles, current: Option[WazuhFiles]): Boolean = {
+  def hasChanges(incoming: WazuhFiles, current: WazuhFiles): Boolean = {
     ???
   }
 
@@ -35,6 +51,9 @@ object Logic {
   // should only be called if the conf has changed and should ideally return the status code
   // TODO: check if we need to poll to find out when restart is complete
   // TODO: check if sudo is needed to restart and work out how to avoid running as root
-  def restartWazuh(): IO[String, Unit] = ???
+  // TODO: Consider if wazup should be responsible for running wazuh-manager entirely
+  def restartWazuh(): IO[String, Unit] = {
+    ???
+  }
 
 }
