6d0c8eb1601490344278a0124c31a37bdfe450c3
Kate Whalen <whalenka88@gmail.com>
2021-05-06 18:12:40 +0100

    Switch to ZIO and use Blocking thread pool

--- src/main/scala/com/gu/wazup/Logic.scala
+++ src/main/scala/com/gu/wazup/Logic.scala
@@ -1,43 +1,82 @@
 package com.gu.wazup
 
+import com.gu.wazup.aws.S3
+import com.typesafe.scalalogging.LazyLogging
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.ssm.SsmAsyncClient
-import zio.IO
+import software.amazon.awssdk.services.ssm.model.{GetParametersByPathRequest, GetParametersByPathResponse}
+import zio.{IO, ZIO}
+import zio.blocking.Blocking
+
+import scala.jdk.CollectionConverters._
+import scala.jdk.FutureConverters._
+import scala.util.control.NonFatal
 
 
-object Logic {
+object Logic  extends LazyLogging {
   // TODO: change left to be a failure so we can return more information
-  def fetchFiles(client: S3Client, bucket: String, prefix: String): IO[String, WazuhFiles] = {
-    ???
+  def fetchFiles(client: S3Client, bucket: String, prefix: String): ZIO[Blocking, String, WazuhFiles] = {
+    for {
+      ossecConf <- S3.getObjectContent(client, bucket, s"$prefix/ossec.conf")
+      decoders <- S3.listObjects(client, bucket, s"$prefix/decoders/")
+      lists <- S3.listObjects(client, bucket, s"$prefix/lists/")
+      rules <- S3.listObjects(client, bucket, s"$prefix/rules/")
+    } yield WazuhFiles(ossecConf, List.empty, List.empty, List.empty)
   }
 
-  def fetchParameters(client: SsmAsyncClient, prefix: String): IO[String, Map[String, String]] = {
-    ???
-
+  // TODO: check pagination =- do we want to use getParametersByPathPaginator?
+  def fetchParameters(client: SsmAsyncClient, prefix: String): IO[String, GetParametersByPathResponse] = {
+    val request = GetParametersByPathRequest.builder()
+      .path(prefix)
+      .withDecryption(true)
+      .recursive(true)
+      .build()
+    ZIO.fromFuture(implicit ec => client.getParametersByPath(request).asScala).refineOrDie {
+      case NonFatal(t) => t.getMessage
+    }
   }
 
-  def parseParameters(parameters: Map[String, String]): WazuhParameters = {
-    ???
+  def parseParameters(response: GetParametersByPathResponse, prefix: String): WazuhParameters = {
+    val parameters = response.parameters.asScala.toList.map { param =>
+      param.name.stripPrefix(prefix) -> param.value
+    }.toMap
+
+    WazuhParameters(
+      // TODO: should missing parameters raise an exception?
+      // what do we want to happen if the parameter is missing?
+      // validate params and notify (CloudWatch log) if
+      parameters.getOrElse("wazuhClusterKey", ""),
+      parameters.getOrElse("coordinatorIP", ""),
+      parameters.getOrElse("agentSecretArn", ""),
+      parameters.get("cloudtrailRoleArn"),
+      parameters.get("guarddutyRoleArn"),
+      parameters.get("umbrellaRoleArn"),
+    )
   }
 
   // TODO: ossec.conf should be valid XML, can we return XML or add a method to validate the conf?
   def createConf(wazuhFiles: WazuhFiles, parameters: WazuhParameters, nodeType: NodeType): IO[String, WazuhFiles] = {
+    // 1. Replace value of 'node_name' with unique identifier
+        // Each node of the cluster must have a unique name.
+        // If two nodes share the same name, one of them will be rejected.
+    // 2. Replace value of 'key' with the clusterKey value
+        // This key must be the same for all of the nodes of the cluster.
+    // 3. Replace value of 'node' with the coordinatorIP value
+
+    // 4. If NodeType is not Leader then removeWodleSections
+
     ???
   }
 
-  // Determine what type of node this is, by one of these options:
-    // 1. Check is our IP matches coordinatorIP (current selection)
-    // 2. Lookup the instance tags for a particular set
-    // 3. lookup value from /etc/nodetype
   def getNodeType(instanceIp: String, parameters: WazuhParameters): NodeType = {
-      instanceIp match {
-        case parameters.coordinatorIP => Leader
-        case _ => Worker
-      }
+    if (instanceIp == parameters.coordinatorIP) Leader
+    else Worker
   }
 
-  // Only one instance should ingest logs from GCP and AWS
-  private[logic] def removeWodleSections(ossecConf: String): String = {
+  // Only the leader instance should ingest logs from GCP and AWS
+  // TODO: example input and outputs in resources
+  def removeWodleSections(ossecConf: String): String = {
+    // remove <wodle name="aws-s3"> and remove <gcp-pubsub>
     ???
   }
 
@@ -48,7 +87,6 @@ object Logic {
     ???
   }
 
-  // if it is the first time the service is running, the current files will be None
   // TODO: consider case class that represents the interpolation
   def hasChanges(incoming: WazuhFiles, current: WazuhFiles): Boolean = {
     ???
--- src/main/scala/com/gu/wazup/aws/S3.scala
+++ src/main/scala/com/gu/wazup/aws/S3.scala
@@ -2,22 +2,28 @@ package com.gu.wazup.aws
 
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.{GetObjectRequest, ListObjectsV2Request, ListObjectsV2Response}
-import zio.RIO
+import zio.ZIO
 import zio.blocking._
 
 import scala.io.Source
+import scala.util.control.NonFatal
 
 
 object S3 {
 
-  def getObjectContent(client: S3Client, bucket: String, key: String): RIO[Blocking, String] = {
+  def getObjectContent(client: S3Client, bucket: String, key: String): ZIO[Blocking, String, String] = {
     val request = GetObjectRequest.builder.bucket(bucket).key(key).build
     effectBlocking(client.getObjectAsBytes(request).asByteArray())
       .map(bytes => Source.fromBytes(bytes, "UTF-8").mkString)
+      .refineOrDie {
+        case NonFatal(t) => t.getMessage
+      }
   }
 
-  def listObjects(client: S3Client, bucket: String, prefix: String): RIO[Blocking, ListObjectsV2Response] = {
+  def listObjects(client: S3Client, bucket: String, prefix: String): ZIO[Blocking, String, ListObjectsV2Response] = {
     val request = ListObjectsV2Request.builder.bucket(bucket).prefix(prefix).build
-    effectBlocking(client.listObjectsV2(request))
+    effectBlocking(client.listObjectsV2(request)).refineOrDie {
+      case NonFatal(t) => t.getMessage
+    }
   }
 }
